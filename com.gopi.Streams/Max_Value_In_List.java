66b734f729356ec65dcf3a8aa80efbc5074a075b
Krishna <gopicovai38@gmail.com>
2024-12-21 16:01:32 +0530

    hai there

--- com.gopi.Streams/Max_Value_In_List.java
+++ com.gopi.Streams/Max_Value_In_List.java
@@ -1,6 +1,7 @@
 public class Max_Value_In_List {
     
-    public static void main(String[] args) {
-       
+    public static void main(String[] args) 
+    {
+         System.out.println("Hai thre");
     }
 }
